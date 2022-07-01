160a19c21deee5e6a18e964657aec88c9ca22d9b
Souradeep Bhattacharya <50011738+ThePsyk3y@users.noreply.github.com>
2022-07-01 11:55:57 +0530

    Improved Code Formatting for Better Readability

--- myheader.h
+++ myheader.h
@@ -1,32 +1,35 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Toy
 {
-    int quantity; string name;
-    int price;bool out_of_stock_flag;
+    int quantity;
+    string name;
+    int price;
+    bool out_of_stock_flag;
 };
 
 class Admin : public Toy
 {
-    string id,password;
-    public:
-        void create_admin(string id_p,string password_p);
-        bool login(string id_p,string password_p);
-        void delete_toy();
-        void update_toy();
-        void create_toy();
+    string id, password;
+
+public:
+    void create_admin(string id_p, string password_p);
+    bool login(string id_p, string password_p);
+    void delete_toy();
+    void update_toy();
+    void create_toy();
 };
 
 class Customer : public Toy
 {
-    string username,password;
-    public :
-        void create_user(string user,string pass);
-        bool login(string id_p,string password_p);
-        void purchase();
-        void view_toys_list();
-        void search_toy(string name);
-        void display_balance(int cash,Toy t);
-        
+    string username, password;
+
+public:
+    void create_user(string user, string pass);
+    bool login(string id_p, string password_p);
+    void purchase();
+    void view_toys_list();
+    void search_toy(string name);
+    void display_balance(int cash, Toy t);
 };
--- mysource.cpp
+++ mysource.cpp
@@ -1,94 +1,93 @@
 #include "myheader.h"
 
-void Admin :: create_admin(string id_p,string password_p)
+void Admin ::create_admin(string id_p, string password_p)
 {
-    id=id_p;
-    password=password_p;
+    id = id_p;
+    password = password_p;
     fstream file;
-    file.open("admin_details.txt",ios::app);
-    file<<id<<" "<<password<<endl;
+    file.open("admin_details.txt", ios::app);
+    file << id << " " << password << endl;
     file.close();
 }
 
-bool Admin :: login(string id_p,string password_p)
+bool Admin ::login(string id_p, string password_p)
 {
-    int tries=0;
-    while(id_p!=id and tries!=3)
+    int tries = 0;
+    while (id_p != id and tries != 3)
     {
         // if()
         // {
-            cout<<"XXXXXXXX  INVALID ADMIN ID. TRY AGAIN!! XXXXXXXXX"<<endl; 
-            tries+=1;
-            if(tries==3)
-            {
-                cout<<"You have entered the incorrect id 3 times in a row. Exiting admin interface"<<endl;
-                return false;
-            }
-            
+        cout << "XXXXXXXX  INVALID ADMIN ID. TRY AGAIN!! XXXXXXXXX" << endl;
+        tries += 1;
+        if (tries == 3)
+        {
+            cout << "You have entered the incorrect id 3 times in a row. Exiting admin interface" << endl;
+            return false;
+        }
+
         // }
     }
-    tries=0;
-    while(tries!=3 and password_p!=password)
+    tries = 0;
+    while (tries != 3 and password_p != password)
     {
         // if()
         // {
-            cout<<"XXXXXXXX  INVALID ADMIN PASSWORD. TRY AGAIN!! XXXXXXXXX"<<endl;
-            tries+=1;
-            if(tries==3)
-            {
-                cout<<"You have entered the incorrect password 3 times in a row. Exiting admin interface"<<endl;
-                return false;
-            }
+        cout << "XXXXXXXX  INVALID ADMIN PASSWORD. TRY AGAIN!! XXXXXXXXX" << endl;
+        tries += 1;
+        if (tries == 3)
+        {
+            cout << "You have entered the incorrect password 3 times in a row. Exiting admin interface" << endl;
+            return false;
+        }
         // }
     }
     return true;
 }
 
-void Customer :: create_user(string user,string pass)
+void Customer ::create_user(string user, string pass)
 {
-    username=user;
-    password=pass;
+    username = user;
+    password = pass;
     fstream file;
-    file.open("customer_details.txt",ios::app);
-    file<<username<<" "<<password<<endl;
+    file.open("customer_details.txt", ios::app);
+    file << username << " " << password << endl;
     file.close();
 }
 
-bool Customer :: login(string id_p,string password_p)
+bool Customer ::login(string id_p, string password_p)
 {
-    int tries=0;
-    while(id_p!=username and tries!=3)
+    int tries = 0;
+    while (id_p != username and tries != 3)
     {
         // if()
         // {
-            cout<<"XXXXXXXX  INVALID USER ID. TRY AGAIN!! XXXXXXXXX"<<endl; 
-            tries+=1;
-            if(tries==3)
-            {
-                cout<<"You have entered the incorrect id 3 times in a row. Exiting user interface"<<endl;
-                return false;
-            }
-            
+        cout << "XXXXXXXX  INVALID USER ID. TRY AGAIN!! XXXXXXXXX" << endl;
+        tries += 1;
+        if (tries == 3)
+        {
+            cout << "You have entered the incorrect id 3 times in a row. Exiting user interface" << endl;
+            return false;
+        }
+
         // }
     }
-    tries=0;
-    while(tries!=3 and password_p!=password)
+    tries = 0;
+    while (tries != 3 and password_p != password)
     {
         // if()
         // {
-            cout<<"XXXXXXXX  INVALID USER PASSWORD. TRY AGAIN!! XXXXXXXXX"<<endl;
-            tries+=1;
-            if(tries==3)
-            {
-                cout<<"You have entered the incorrect password 3 times in a row. Exiting user interface"<<endl;
-                return false;
-            }
+        cout << "XXXXXXXX  INVALID USER PASSWORD. TRY AGAIN!! XXXXXXXXX" << endl;
+        tries += 1;
+        if (tries == 3)
+        {
+            cout << "You have entered the incorrect password 3 times in a row. Exiting user interface" << endl;
+            return false;
+        }
         // }
     }
     return true;
 }
 
-void Customer :: display_balance(int cash,Toy t)
+void Customer ::display_balance(int cash, Toy t)
 {
-    
 }
