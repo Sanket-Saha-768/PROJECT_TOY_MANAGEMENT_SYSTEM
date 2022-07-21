033c267767c03c0a0b65c27358c0e6067f1adf57
Souradeep Bhattacharya <50011738+ThePsyk3y@users.noreply.github.com>
2022-07-21 23:04:28 +0530

    random changes

--- main.cpp
+++ main.cpp
@@ -7,38 +7,21 @@ int main()
 
     switch (generalMenu())
     {
-    case 1:{
-        cout<<"\n-------------------\nFor admin :"<<endl;
-        a1.login_menu();
+    case 1:
+    {
+        cout << "\n-------------------\nFor admin :" << endl;
+        // a1.login_menu();
     }
-        break;
+    break;
     case 2:
-        c1.login_menu();
+        // c1.login_menu();
         break;
     case 0:
         exit(0);
         break;
     default:
-        cout<<"\nIncorrect Input!\n";
+        cout << "\nIncorrect Input!\n";
         break;
     }
-
-    // fstream file;
-    // file.open("admin_details.txt",ios::in);
-    // string s;
-    // // getline(file,s);
-    // // // getline(file,s,' ');cout<<s<<endl;
-    // // // getline(file,s,' ');cout<<s<<endl;
-    // c1.view_toys_list();
-    // // a1.create_toy();
-    // // a1.create_toy();
-    // // a1.create_toy();
-    // // a1.create_toy();
-    // // a1.create_toy();
-    // // a1.create_toy();
-    // // a1.create_toy();
-    // // a1.create_toy();
-    // // a1.create_toy();
-    // c1.search_toy();    
     return 0;
 }
